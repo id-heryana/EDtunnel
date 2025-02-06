0ce399d6f11ac2b7b0bf725dd0f1e979e00c0395
gopaybis <vlessgithub1@outlook.co.id>
2025-02-07 00:57:28 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '8318ba19-e625-4727-8950-218912f34f13';
 
 const พร็อกซีไอพีs = ['104.248.145.216'];
 
